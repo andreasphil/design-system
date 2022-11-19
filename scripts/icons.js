8eae75174622a1458e11b51ce00bc0cb43b028f6
Andreas Philippi <18646892+andreasphil@users.noreply.github.com>
2022-11-19 08:40:15 +0100

    fix(icons): icons should now also respect [data-theme]

--- scripts/icons.js
+++ scripts/icons.js
@@ -1,21 +1,37 @@
+// @ts-check
+
 import { readdirSync, readFileSync, writeFileSync } from "fs";
 import { resolve } from "path";
 
-/** Template for the output SCSS */
-const scss = ([light, dark]) => `
-/* This file is generated with \`node scripts/icons.js\` */
-:root {
-${light.trim()}
+/**
+ * Template for the output SCSS.
+ * @param {string[]} light
+ * @param {string[]} dark
+ * @returns {string}
+ */
+function scss(light, dark) {
+  return `/* This file is generated with \`node scripts/icons.js\` */
+@mixin lightIcons() {
+${light.join("\n").trim()}
 }
-@media (prefers-color-scheme: dark) { :root {
-${dark.trim()}
-}}`;
+@mixin darkIcons() {
+${dark.join("\n").trim()}
+}`;
+}
 
-/** Convert an SVG string into a data URI */
-const dataUri = (source) =>
-  `data:image/svg+xml;charset=utf-8,${encodeURIComponent(source)}`;
+/**
+ * Convert an SVG string into a data URI
+ * @param {string} source
+ * @returns {string}
+ */
+function dataUri(source) {
+  return `data:image/svg+xml;charset=utf-8,${encodeURIComponent(source)}`;
+}
 
-/** Shorthand for getting absolute file paths */
+/**
+ * Shorthand for getting absolute file paths.
+ * @param {string} filename
+ */
 function path(filename) {
   const dirname = new URL("..", import.meta.url).pathname;
   return resolve(dirname, "src/icons", filename);
@@ -36,11 +52,17 @@ const icons = readdirSync(path("."))
   })
   .reduce(
     // Group all light and dark icons together and convert them into data URIs
-    ([light, dark], [name, lightIcon, darkIcon]) => [
-      `${light}--icon-${name}: url("${dataUri(lightIcon)}");\n`,
-      `${dark}--icon-${name}: url("${dataUri(darkIcon)}");\n`,
-    ],
-    ["", ""]
+    (
+      /** @type Record<string, string[]> */ all,
+      [name, lightIcon, darkIcon]
+    ) => {
+      all.light.push(`--icon-${name}: url("${dataUri(lightIcon)}");`);
+      all.dark.push(`--icon-${name}: url("${dataUri(darkIcon)}");`);
+      return all;
+    },
+    { light: [], dark: [] }
   );
 
-writeFileSync(path("_icons.scss"), scss(icons), { encoding: "utf-8" });
+writeFileSync(path("_icons.scss"), scss(icons.light, icons.dark), {
+  encoding: "utf-8",
+});
